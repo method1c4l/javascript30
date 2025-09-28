51307d1a2d8b6575db35a841bcf2da5c5098af61
method1c4l <pablodavid31@outlook.com>
2025-09-28 16:34:19 -0300

    Add first project

---
+++ drum-kit/src/js/script.js
@@ -0,0 +1,25 @@
+document.addEventListener('keydown', playSound);
+
+function playSound(event) {
+    const audio = document.querySelector(`audio[data-key="${event.keyCode}"]`);
+    const key = document.querySelector(`.key[data-key="${event.keyCode}"]`);
+
+    if (!audio) return;
+
+    audio.currentTime = 0;
+
+    audio.play();
+
+    key.classList.add('playing');
+}
+
+
+const keys = document.querySelectorAll('.key');
+
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+
+function removeTransition(event) {
+    if (event.propertyName !== 'transform') return;
+
+    this.classList.remove('playing');
+}
